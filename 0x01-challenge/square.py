c59942912c09298e717bd4f2c0ea228ec711ec6c
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-11 14:37:05 +0100

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+"""
+A module for a class square
+"""
 
-class square():
-    
+
+class Square():
+    """
+    A class for a square
+    """    
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """
+        A method for a class square
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,15 +23,21 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """
+        A method for a class square
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        A method for a class square
+        """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
